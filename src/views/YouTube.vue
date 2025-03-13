4a35746a262455df6473e9455fcc6107a4ca56ed
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-03-13 12:14:23 -0500

    Added content to youtube page

--- src/views/YouTube.vue
+++ src/views/YouTube.vue
@@ -1,17 +1,128 @@
 <template>
-    <div class="grid place-content-center border-t border-base-300 h-96">
-        <div class="">
-            <div class="hero bg-base-200 h-80">
-                <div class="hero-content text-center">
-                    <div class="max-w-md">
-                        <h1 class="text-5xl font-bold">I make videos</h1>
-                        <p class="py-6">
-                            Educational Dota 2 content on YouTube.
-                            My goal is to help you improve at Dota 2.
-                        </p>
-                        <button class="btn btn-primary">Check it out!</button>
+    <div class="max-w-2xl w-full m-auto p-6">
+        <div class="flex flex-col">
+            <h2 class="py-2 text-2xl">YouTube</h2>
+            <!-- Stats -->
+            <div class="stats shadow mb-5 bg-[#ffd385]">
+                <div class="stat">
+                    <div class="stat-figure text-primary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            class="inline-block h-8 w-8 stroke-error">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
+                            </path>
+                        </svg>
+                    </div>
+                    <div class="stat-title text-error">Total Views</div>
+                    <div class="stat-value text-error">4.8M</div>
+                    <!-- <div class="stat-desc">21% more than last month</div> -->
+                </div>
+
+                <div class="stat">
+                    <div class="stat-figure text-secondary">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            class="inline-block h-8 w-8 stroke-info">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+                        </svg>
+                    </div>
+                    <div class="stat-title text-info">Watch Hours</div>
+                    <div class="stat-value text-info">308k</div>
+                    <!-- <div class="stat-desc">21% more than last month</div> -->
+                </div>
+
+                <div class="stat">
+                    <div class="stat-figure text-secondary">
+                        <div class="avatar online">
+                            <div class="w-16 rounded-full">
+                                <a href="https://www.youtube.com/channel/UCO3DiGgdcpWcfuu9cZYG_JA" target="_blank">
+                                    <img
+                                        src="https://yt3.googleusercontent.com/iFUxf8AszvLbJrSJTVWyW6SYvwAYxHuXh-IdDisyd5B_pE3Q4zhDkATp8Q4Y08D9ovdtkDpp3w=s160-c-k-c0x00ffffff-no-rj" />
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="stat-title text-success">Subscribers</div>
+                    <div class="stat-value text-success">36.4k</div>
+                    <!-- <div class="stat-desc text-secondary">31 tasks remaining</div> -->
+                </div>
+            </div>
+            <!-- Stats end -->
+
+            <div class="">
+                <img src="https://i.pinimg.com/originals/97/21/e1/9721e113b62d2602ee3ebb6a82027538.gif"
+                    alt="Anime camera zooming gif"
+                    class="w-4/5 h-full rounded float-none m-auto mb-2 xs:float-right xs:ml-3 xs:mb-3 xs:w-45">
+                <p class="font-normal mb-3">I create in-depth, educational Dota 2 content on YouTube, breaking down
+                    complex widely misunderstood or interesting mechanics and strategies to help players improve. With a
+                    focus on clarity and insight, I make videos
+                    that analyze pro gameplay, explore key concepts, and share practical tips.
+                </p>
+                <p class="font-normal">Beyond Dota, I'm
+                    passionate about content creation itself—constantly refining my editing, storytelling, and
+                    production skills. My goal is to make learning engaging and accessible while building a community of
+                    like-minded players who love improving their game.
+                </p>
+            </div>
+
+            <!-- Channel image -->
+            <h2 class="mt-5 pb-2 text-2xl">100+ Educational Videos</h2>
+            <a href="https://www.youtube.com/@zzadrianzz/videos" target="_blank">
+                <div class="parallax-section rounded">
+                    <div class="parallax-image"></div>
+                </div>
+            </a>
+
+            <!-- Streams -->
+            <h2 class="mt-5 pb-2 text-2xl">Streams</h2>
+            <div>
+                <div
+                    class="text-center w-4/5 h-full rounded float-none m-auto mb-2 xs:float-left xs:mr-3 xs:mb-3 xs:w-45">
+                    <p class="mb-2">Watch on..</p>
+                    <div class="flex gap-2 items-center justify-center">
+                        <a href="https://www.twitch.tv/zzadrianzz_" target="_blank">
+                            <img src="https://www.svgrepo.com/show/354473/twitch.svg" alt="" class="w-12">
+                        </a>
+                        or
+                        <a href="https://www.youtube.com/@zzadrianzz/live" target="_blank">
+                            <img src="https://www.svgrepo.com/show/475700/youtube-color.svg" alt="" class="w-15">
+                        </a>
                     </div>
                 </div>
+                <p class="font-normal">I stream Dota 2 on Twitch and YouTube, focusing on high-level gameplay,
+                    analysis,
+                    and
+                    interactive
+                    learning. Whether
+                    I'm breaking down decisions, trying new strategies, or just having fun, I engage with my
+                    community
+                    in a
+                    way that makes improving at the game both enjoyable and insightful.</p>
+            </div>
+
+
+            <!-- Memberships -->
+            <h2 class="mt-5 pb-2 text-2xl">Join The Community</h2>
+            <div>
+                <div
+                    class="text-center w-4/5 h-full rounded float-none m-auto mb-2 xs:float-right xs:ml-3 xs:mb-3 xs:w-45">
+                    <p class="mb-2">Support on..</p>
+                    <div class="flex gap-2 items-center justify-center">
+                        <a href="https://www.patreon.com/zzadrianzz" target="_blank">
+                            <img src="https://brandlogos.net/wp-content/uploads/2023/10/patreon-logo_brandlogos.net_1pcmb.png"
+                                alt="" class="w-11">
+                        </a>
+                        or
+                        <a href="https://www.youtube.com/@zzadrianzz/join" target="_blank">
+                            <img src="https://www.svgrepo.com/show/475700/youtube-color.svg" alt="" class="w-15">
+                        </a>
+                    </div>
+                </div>
+                <p class="font-normal">For those who want to dive deeper, I offer exclusive content and extra perks
+                    through my Patreon and YouTube Memberships. Members get access to behind-the-scenes insights,
+                    in-depth hero learning videos, and special community events. I also provide coaching services for
+                    those looking to improve their Dota gameplay with personalized feedback. It's a great way to support
+                    my work while getting even more value in return.</p>
             </div>
         </div>
     </div>
@@ -23,4 +134,22 @@ export default {
 }
 </script>
 
-<style></style>
+<style scope>
+.parallax-section {
+    position: relative;
+    width: 100%;
+    height: 250px;
+    overflow: hidden;
+}
+
+.parallax-image {
+    background-image: url("https://i.ibb.co/rf33z4Kx/Screenshot-2025-03-13-at-11-38-38-AM.png");
+    background-size: 57.5% auto;
+    /* Zooms into the image horizontally */
+    background-position: center 10%;
+    /* Moves the image to show the desired crop */
+    background-attachment: fixed;
+    width: 100%;
+    height: 100%;
+}
+</style>
