295ddda8405a55386711f51035865ec0dbaddb62
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-03-13 10:33:34 -0500

    Added content to swe page

--- src/views/SoftwareEngineer.vue
+++ src/views/SoftwareEngineer.vue
@@ -1,19 +1,161 @@
 <template>
-    <div class="grid place-content-center border-t border-base-300 h-96">
-        <div class="">
-            <div class="hero bg-base-200 h-80">
-                <div class="hero-content text-center">
-                    <div class="max-w-md">
-                        <h1 class="text-5xl font-bold">Coooooode</h1>
-                        <p class="py-6">
-                            I'm a software engineer with a high attention to detail.
-                            I'll learn anything to get the job done.
-                        </p>
-                        <button class="btn btn-primary">GitHub</button>
-                    </div>
-                </div>
+    <div class="max-w-2xl w-full m-auto p-6">
+        <div class="flex flex-col">
+            <h2 class="py-3 text-2xl">Software Engineer</h2>
+            <div class="">
+                <img src="https://i.pinimg.com/originals/87/df/6d/87df6d60f4cc3c07968ae2127bddcc30.gif"
+                    alt="Anime typing on computer gif"
+                    class="w-4/5 h-full rounded float-none m-auto mb-2 xs:float-left xs:mr-3 xs:mb-3 xs:w-45">
+                <p class="font-normal mb-3">I'm a software engineer with a strong focus on frontend development,
+                    building
+                    intuitive and efficient applications. I enjoy creating tools that solve real problems, whether it's
+                    for gaming, content creation, or productivity.
+                </p>
+                <p class="font-normal">My experience includes working with modern web
+                    technologies like JavaScript, TypeScript, Vue, and React, along with backend integration and API
+                    development. Beyond coding, I'm always learning—exploring new frameworks, optimizing workflows, and
+                    staying up to date with industry trends. I take pride in writing clean, maintainable code and
+                    building projects that are both functional and engaging.
+                </p>
             </div>
         </div>
+
+        <h2 class="py-3 text-2xl">What I've Worked On</h2>
+        <div class="flex flex-col">
+            <div class="bg-[#5f886d] p-3 text-success-content rounded-t">
+                <table>
+                    <tr>
+                        <td class="w-17">
+                            <img src="https://i.ibb.co/gzfSzMX/resoundfm-logo.jpg" alt="Resound Logo"
+                                class="mr-4 w-12 rounded" />
+                        </td>
+                        <td>
+                            <a href="https://www.resound.fm/" target="_blank"><span
+                                    class="underline font-semibold">Resound</span></a>
+                            • <i>Full Stack
+                                Software Engineer</i><br />
+                            <span>Artificial Intelligence podcast editing tool</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="bg-[#8fa17b] p-3">
+                <table>
+                    <tr>
+                        <td class="w-17">
+                            <img src="https://i.ibb.co/SyK5x1g/-logo.png" alt=" Recordings Logo"
+                                class="mr-4 w-12 rounded-full" />
+                        </td>
+                        <td>
+                            <a href="https://recordings.com/" target="_blank"><span class="underline font-semibold">
+                                    Recordings</span></a> • <i>Full Stack Software Engineer</i><br />
+                            <span>Podcast editing services and production platform</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="bg-[#baa365] p-3">
+                <table>
+                    <tr>
+                        <td class="w-17">
+                            <img src="https://www.iconpacks.net/icons/2/free-reddit-logo-icon-2436-thumb.png"
+                                alt="Reddit Logo" class="mr-4 w-12" />
+                        </td>
+                        <td>
+                            <a href="https://reddit-reader-wow.netlify.app/" target="_blank"><span
+                                    class="underline font-semibold">Reddit
+                                    Reader</span></a> • <i>Personal Project</i><br />
+                            <span>View posts that you care about from your favorite subreddit</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div class="bg-[#f09d30] p-3">
+                <table>
+                    <tr>
+                        <td class="w-17">
+                            <img src="https://i.ibb.co/C95g9Jb/icons8-music-120.png" alt="Higher Lower Music Logo"
+                                class="mr-4 w-12 rounded" />
+                        </td>
+                        <td>
+                            <span class="font-semibold">Higher Lower Music</span> • <i>Personal Project</i><br />
+                            <span>Higher lower game based on music artists</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+
+            <div class="bg-[#fdc873] p-3">
+                <table>
+                    <tr>
+                        <td class="w-17">
+                            <a href="https://zzadrianzz.github.io/PomodoroTimer/" target="_blank">
+                                <img src="https://raw.githubusercontent.com/zzADRiANzz/PomodoroTimer/main/images/wen-small-meow-cat-small.png"
+                                    alt="Pomodoro Timer Logo" class="mr-4 w-12" /></a>
+                        </td>
+                        <td>
+                            <a href="https://zzadrianzz.github.io/PomodoroTimer/" target="_blank"><span
+                                    class="underline font-semibold">Pomodoro
+                                    Timer</span></a> • <i>Personal Project</i><br />
+                            <span>Simple and cute Pomodoro timer</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+
+            <div class="bg-[#f5906c] p-3 rounded-b">
+                <table>
+                    <tr>
+                        <td class="w-17">
+                            <img src="https://i.ibb.co/GVsSHPS/Screenshot-2024-08-28-at-10-34-59-AM.png"
+                                alt="Twitch Chat Hangman Logo" class="mr-4 w-12 rounded" />
+                        </td>
+                        <td>
+                            <a href="https://zzadrianzz.github.io/TwitchChatHangman/" target="_blank"><span
+                                    class="underline font-semibold">Twitch Chat
+                                    Hangman</span></a> • <i>Personal Project</i><br />
+                            <span>Classic hangman game with Twitch chat</span>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+        </div>
+
+        <div class="flex mt-8 justify-around items-center">
+            <div class="text-center">
+                <img src="https://www.svgrepo.com/show/489645/cv.svg" alt="" class="w-15 m-auto" />
+                <a href="https://docs.google.com/document/d/1eRTbrDgA-D9b4-t_rsoL2Ou0hqacmP3K3Cpe8MS3n6c/edit?usp=sharing"
+                    target="_blank" class="btn btn-primary mt-2 cursor-pointer inline-block px-4 py-2 rounded">
+                    Resume
+                </a>
+            </div>
+
+            <div class="text-center">
+                <img src="https://www.svgrepo.com/show/343567/linkedin-network-communication-connection-internet-online.svg"
+                    alt="" class="w-15 m-auto" />
+                <a href="https://www.linkedin.com/in/adrian-rodriguez-dev/" target="_blank"
+                    class="btn btn-accent mt-2 cursor-pointer inline-block px-4 py-2 rounded">
+                    LinkedIn
+                </a>
+
+            </div>
+
+            <div class="text-center">
+                <img src="https://www.svgrepo.com/show/353783/github-octocat.svg" alt="" class="w-15 m-auto" />
+                <a href="https://github.com/adrianistaken" target="_blank"
+                    class="btn btn-info mt-2 cursor-pointer inline-block px-4 py-2 rounded">
+                    GitHub
+                </a>
+            </div>
+        </div>
+
+        <!-- <router-link to="/about"><button class="btn btn-info">Learn more about me</button></router-link> -->
     </div>
 </template>
 
