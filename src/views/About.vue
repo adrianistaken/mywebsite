75d66a398e407fb6a4de1e1b04d8ca0d3b8d59fd
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-03-13 12:15:20 -0500

    Remove temp content on about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="max-w-lg w-full m-auto p-6">
         <h1 class="text-5xl font-bold">Who am I?</h1>
-        <p class="py-3">
+        WIP DON'T LOOK
+        <!-- <p class="py-3">
             Hey! I'm Adrian, a tech enthusiast who loves to tinker and build cool stuff. I spend most of
             my days creating things, whether that's coding, YouTube videos, or game development. I'm all
             about making things that not only look good but feel great to use/consume.
         </p>
         <h2 class="text-3xl font-bold">Free time?</h2>
-
         <p class="py-3">
             When I give myself some free time, you can probably find me playing Dota 2. I run an
             educational YouTube channel where I dive deep into widely misunderstood and intersting
@@ -19,9 +19,9 @@
             development trick. I like to think of myself as a lifelong learner with a bit of a creative
             streak. If you're the same, let's chat about it! I love learning from others :)
         </p>
-        <h2 class="text-3xl font-bold">Connect with me</h2>
+        <h2 class="text-3xl font-bold">Connect with me</h2> -->
 
-        <button class="btn btn-primary">Random button</button>
+        <!-- <button class="btn btn-primary">Random button</button> -->
     </div>
 </template>
 
