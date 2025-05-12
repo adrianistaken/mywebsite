e5952a2a27111e4d3a63d97853ea6bc78859ff34
adrianistaken <adrian.rodriguez.dev@gmail.com>
2025-05-12 10:57:37 -0500

    Update footer

--- src/views/App.vue
+++ src/views/App.vue
@@ -59,8 +59,9 @@ const route = useRoute();
             <!-- Footer -->
             <footer class="footer footer-horizontal footer-center bg-success text-base-content p-10 mt-auto">
                 <nav>
-                    <div class="grid grid-flow-col gap-4">
-                        <a href="https://x.com/zzadrianzz_" target="_blank">
+                    <div class="grid grid-flow-col gap-4 text-success-content text-2xl">
+                        thanks for stopping by! :)
+                        <!-- <a href="https://x.com/zzadrianzz_" target="_blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 class="fill-base-300">
                                 <path
@@ -83,7 +84,7 @@ const route = useRoute();
                                     d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z">
                                 </path>
                             </svg>
-                        </a>
+                        </a> -->
                     </div>
                 </nav>
             </footer>
